b06483f9d10d7bdb71b94bbab5ab289ce4b1d937
Jaid <6216144+Jaid@users.noreply.github.com>
2023-07-25 23:19:22 +0200

    Testing
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

---
+++ .github/actions/build/index.js
@@ -0,0 +1,19 @@
+// @ts-nocheck
+import core from '@actions/core'
+import yaml from 'js-yaml'
+import fs from 'fs-extra'
+import path from 'path'
+import readFileYaml from 'read-file-yaml'
+import {globbyStream} from 'globby'
+const github = JSON.parse(process.env.github)
+const globbyIterator = globbyStream('*.yml', {
+  cwd: path.resolve(github.workspace, 'src'),
+})
+for await (const file of globbyIterator) {
+  console.dir(file)
+  const data = await readFileYaml(file)
+  console.dir(data)
+  const output = yaml.dump(data)
+  const outputFile = path.resolve(github.workspace, 'dist', file)
+  await fs.outputFile(outputFile, output)
+}
