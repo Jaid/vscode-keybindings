ba839414fab050432643a9defae22603891958de
Jaid <6216144+Jaid@users.noreply.github.com>
2023-07-26 15:34:54 +0200

    Testing

--- .github/actions/build/index.js
+++ .github/actions/build/index.js
@@ -15,6 +15,13 @@ for await (const globbyEntry of globbyIterator) {
   console.dir(globbyEntry)
   const data = await readFileYaml.default(globbyEntry.path)
   console.dir(data)
+  const newOutput = []
+  for (const entry of data) {
+    if (entry.command.startsWith('-')) {
+      continue
+    }
+    newOutput.push(entry)
+  }
   const output = yaml.dump(data)
   const outputFile = path.resolve(github.workspace, 'dist', file)
   await fs.outputFile(outputFile, output)
