1198132dedba9477b4ea98e5971c63caed5aef00
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-08 00:25:42 +0200

    Testing

--- .github/actions/convertGlobals/index.js
+++ .github/actions/convertGlobals/index.js
@@ -4,6 +4,7 @@ import fs from 'fs-extra'
 import yaml from 'yaml'
 import path from 'path'
 import readFileJson from 'read-file-json'
+import KeyCounter from 'key-counter'
 const excludedKeystrokes = [
   'escape escape',
   /^(|(ctrl|alt|shift|ctrl\+shift|ctrl\+shift\+alt)\+)(up|left|right|down)$/,
@@ -15,6 +16,14 @@ const jsonPath = path.resolve(github.workspace, 'src', 'global.jsonc')
 const data = await readFileJson.default(jsonPath)
 const result = []
 const excluded = []
+const exclusionCounter = new KeyCounter
+const toYaml = input => yaml.stringify(input, null, {
+  schema: 'core',
+  lineWidth: 0,
+  minContentWidth: 0,
+  singleQuote: true,
+  nullStr: '~'
+})
 const shouldInclude = entry => {
   if (entry.command.startsWith('-')) {
     return false
@@ -22,11 +31,13 @@ const shouldInclude = entry => {
   for (const excludedKeystroke of excludedKeystrokes) {
     if (typeof excludedKeystroke === 'string') {
       if (entry.key === excludedKeystroke) {
+        exclusionCounter.feed(excludedKeystroke)
         return false
       }
     }
     if (excludedKeystroke instanceof RegExp) {
       if (excludedKeystroke.test(entry.key)) {
+        exclusionCounter.feed(excludedKeystroke.source)
         return false
       }
     }
@@ -43,22 +54,12 @@ for (const entry of data) {
 }
 core.info(`Loaded ${Object.keys(data).length} global keybindings from ${jsonPath}`)
 core.info(`Included ${result.length}, excluded ${data.length - result.length}`)
-const yamlString = yaml.stringify(result, null, {
-  schema: 'core',
-  lineWidth: 0,
-  minContentWidth: 0,
-  singleQuote: true,
-  nullStr: '~'
-})
+const yamlString = toYaml(result)
 core.startGroup('YAML output')
 core.info(yamlString)
 core.endGroup()
 core.startGroup('Excluded')
-core.info(yaml.stringify(excluded, null, {
-  schema: 'core',
-  lineWidth: 0,
-  minContentWidth: 0,
-  singleQuote: true,
-  nullStr: '~'
-}))
+core.info(`Excluded ${excluded.length} keybindings`)
+core.info(toYaml(exclusionCounter.toObjectSortedByValues()))
+core.info(toYaml(excluded))
 core.endGroup()
