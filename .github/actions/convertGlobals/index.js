ff26a184eb1261bf14533a02bffa48e8d3ab9700
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-07 03:58:38 +0200

    Testing

--- .github/actions/convertGlobals/index.js
+++ .github/actions/convertGlobals/index.js
@@ -1,9 +1,16 @@
 // @ts-nocheck
 import fs from 'fs-extra'
+import yaml from 'yaml'
 import path from 'path'
 import readFileJson from 'read-file-json'
 const github = JSON.parse(process.env.github)
 const jsonPath = path.resolve(github.workspace, 'src', 'global.jsonc')
 const data = await readFileJson.default(jsonPath)
-console.dir(data)
-// await fs.outputFile(path.resolve(github.workspace, 'dist', "package.json"), JSON.stringify(packageManifest))
+const yamlString = yaml.stringify(data, null, {
+  schema: 'core',
+  doubleQuotedMinMultiLineLength: 10000,
+  lineWidth: 0,
+  minContentWidth: 0,
+  singleQuote: true
+})
+console.log(yamlString)
