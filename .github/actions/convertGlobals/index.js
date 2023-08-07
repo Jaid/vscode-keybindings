11cc5a0f92ff18059de4842a8f5e9f42c514580b
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-07 05:03:51 +0200

    Testing

--- .github/actions/convertGlobals/index.js
+++ .github/actions/convertGlobals/index.js
@@ -1,21 +1,20 @@
 // @ts-nocheck
+import core from '@actions/core'
 import fs from 'fs-extra'
 import yaml from 'yaml'
 import path from 'path'
 import readFileJson from 'read-file-json'
 const github = JSON.parse(process.env.github)
 const jsonPath = path.resolve(github.workspace, 'src', 'global.jsonc')
-// const data = await readFileJson.default(jsonPath)
-const data = {
-  abc: 'def\nghi\njkl\nmno\npqr\nstu\nvwx\nyz'
-}
+const data = await readFileJson.default(jsonPath)
+core.info(`Loaded ${Object.keys(data).length} global keybindings from ${jsonPath}`)
 const yamlString = yaml.stringify(data, null, {
   schema: 'core',
-  doubleQuotedMinMultiLineLength: 10000,
   lineWidth: 0,
   minContentWidth: 0,
   singleQuote: true,
-  nullStr: '~',
-  blockQuote: 'literal'
+  nullStr: '~'
 })
-console.log(yamlString)
+core.startGroup('YAML output')
+core.info(yamlString)
+core.endGroup()
