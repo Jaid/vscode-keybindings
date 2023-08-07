52aa2738f0f3f5416e9a83a5514800b42f2efcd2
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-07 03:50:44 +0200

    Testing

--- .github/actions/convertGlobals/index.js
+++ .github/actions/convertGlobals/index.js
@@ -3,7 +3,7 @@ import fs from 'fs-extra'
 import path from 'path'
 import readFileJson from 'read-file-json'
 const github = JSON.parse(process.env.github)
-const jsonPath = path.resolve(github.workspace, 'src', 'keybindings.json')
+const jsonPath = path.resolve(github.workspace, 'src', 'global.jsonc')
 const data = await readFileJson.default(jsonPath)
 console.dir(data)
 // await fs.outputFile(path.resolve(github.workspace, 'dist', "package.json"), JSON.stringify(packageManifest))
