e0559e0281e39a7c81eacc803a97e0dcd6e41c2b
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-10 04:52:20 +0200

    Testing

--- .github/actions/buildHtml/index.js
+++ .github/actions/buildHtml/index.js
@@ -2,5 +2,25 @@
 import {context} from '@actions/github'
 import fs from 'fs-extra'
 import path from 'path'
-const outputPath = path.resolve(process.env.GITHUB_WORKSPACE, 'dist', "index.html")
-await fs.outputFile(outputPath, "test")
+import showdown from 'showdown'
+import Handlebars from 'handlebars'
+import * as url from 'url';
+import {fileURLToPath} from "node:url"
+
+const dirName = path.dirname(fileURLToPath(import.meta.url))
+
+const handlebars = Handlebars.create()
+const template = await readFileString(path.resolve(dirName, 'template.hbs'))
+const templateInvoker = handlebars.compile(template)
+const md = templateInvoker()
+const converter = new showdown.Converter
+const html = converter.makeHtml(md)
+const outputFolder = path.resolve(process.env.GITHUB_WORKSPACE, 'dist')
+const mdFile = path.resolve(outputFolder, 'index.md')
+const htmlFile = path.resolve(outputFolder, 'index.html')
+await fs.ensureDir(outputFolder)
+const outputJobs = [
+  fs.writeFile(mdFile, md),
+  fs.writeFile(htmlFile, html)
+]
+await Promise.all(outputJobs)
