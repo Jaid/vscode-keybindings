619e2daff1616573d010a1e861cad25b402baa4c
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-10 07:33:05 +0200

    Testing

--- .github/actions/build/index.ts
+++ .github/actions/build/index.ts
@@ -2,6 +2,11 @@
 import core from '@actions/core'
 // import toYaml from './lib/toYaml'
 
+import {fileURLToPath} from "node:url"
+
+const dirName = path.dirname(fileURLToPath(import.meta.url))
+console.log(dirName)
+
 core.info(toYaml({
   moin: 'servus'
 }))
