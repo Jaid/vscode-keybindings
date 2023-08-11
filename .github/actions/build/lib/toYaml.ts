591f558a06eafadd1de6af9bee8814d886ab67ae
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-11 08:49:15 +0200

    Testing

--- .github/actions/build/lib/toYaml.ts
+++
@@ -1,9 +0,0 @@
-import yaml from 'yaml'
-
-export default (input): string => yaml.stringify(input, null, {
-  schema: 'core',
-  lineWidth: 0,
-  minContentWidth: 0,
-  singleQuote: true,
-  nullStr: '~'
-})
