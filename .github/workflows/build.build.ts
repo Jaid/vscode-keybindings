78eeceabb37b603a04b275209473e2bdf2919ecf
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-11 08:33:18 +0200

    Testing

--- .github/workflows/build.build.ts
+++ .github/workflows/build.build.ts
@@ -1,22 +1,29 @@
 // @ts-nocheck
-import core from '@actions/core'
-import path from 'node:path'
+import readFileYaml from 'read-file-yaml'
+import {globbyStream} from 'globby'
+import * as core from '@actions/core'
 
-import yaml from 'yaml'
-
-const toYaml = input => yaml.stringify(input, null, {
-  schema: 'core',
-  lineWidth: 0,
-  minContentWidth: 0,
-  singleQuote: true,
-  nullStr: '~'
+const globbyIterator = globbyStream('*.yml', {
+  cwd: 'src',
+  objectMode: true,
+  absolute: true
 })
-
-import {fileURLToPath} from "node:url"
-
-const dirName = path.dirname(fileURLToPath(import.meta.url))
-console.log(dirName)
-
-core.info(toYaml({
-  moin: 'servus'
-}))
+const additions = []
+const deletions = []
+for await (const globbyEntry of globbyIterator) {
+  console.dir(globbyEntry)
+  const data = await readFileYaml.default(globbyEntry.path)
+  console.dir(data)
+  for (const entry of data) {
+    if (entry.command.startsWith('-')) {
+      deletions.push(entry)
+      continue
+    }
+    additions.push(entry)
+  }
+}
+const output = {
+  ...deletions,
+  ...additions
+}
+core.setOutput('value', JSON.stringify(output))
