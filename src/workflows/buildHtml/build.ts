b09b511b96349ed4425473461b3dad316c5c9549
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-13 15:48:29 +0200

    Testing

--- src/workflows/buildHtml/build.ts
+++ src/workflows/buildHtml/build.ts
@@ -11,7 +11,7 @@ import readFileString from 'read-file-string'
 import readFileYaml from 'read-file-yaml'
 import showdown from 'showdown'
 
-import {Keybinding, RawKeybinding} from 'lib/Keybinding.ts'
+import {Keybinding, RawKeybinding} from 'src/workflows/lib/Keybinding.ts'
 
 const dirName = path.dirname(fileURLToPath(import.meta.url))
 
