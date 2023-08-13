ed14f441fa866f68a87edf0a4af9211585b0c1da
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-13 11:14:30 +0200

    Testing

---
+++ src/workflows/lib/Keybinding.ts
@@ -0,0 +1,21 @@
+export interface RawKeybinding {
+  key: string
+  command: string
+  args?: string[]
+  when?: string
+}
+
+export default class Keybinding {
+
+  key: any
+
+  command: any
+
+  args: any
+
+  constructor(public data: RawKeybinding) {
+    this.key = data.key
+    this.command = data.command
+    this.args = data.args
+  }
+}
