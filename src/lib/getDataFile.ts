2909c8eb685d3028f0a29308d25138efdfc3ec2d
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-25 06:31:29 +0200

    Testing

--- src/lib/getDataFile.ts
+++ src/lib/getDataFile.ts
@@ -2,8 +2,9 @@ import path from 'node:path'
 
 export default () => {
   if (process.env.dataFile) {
+    console.log(`$(path.join(process.env.dataFile, 'out', 'data.yml'))`)
     return path.join(process.env.dataFile, `out`, `data.yml`)
-  } else {
-    return path.join(`out`, `data.yml`)
   }
+  console.log(`$(path.join('out', 'data.yml'))`)
+  return path.join(`out`, `data.yml`)
 }
