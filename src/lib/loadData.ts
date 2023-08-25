f4fb8ce49033a41d0acc2685daefa3fa453b9d78
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-25 06:46:33 +0200

    Testing

--- src/lib/loadData.ts
+++ src/lib/loadData.ts
@@ -1,10 +1,10 @@
 import path from 'node:path'
 
-import core from '@actions/core'
+import fs from 'fs-extra'
 import readFileYaml from 'read-file-yaml'
 
 import getDataFile from 'lib/getDataFile.js'
-import {Keybinding, KeyVisualization, RawKeybinding} from 'lib/Keybinding.js'
+import {Keybinding, KeyVisualization} from 'lib/Keybinding.js'
 
 import {Data} from 'src/workflows/build/build.js'
 
@@ -13,6 +13,8 @@ export type ExtendedKeybinding = Keybinding & {visualization: KeyVisualization}
 export default async () => {
   const data: Data = await readFileYaml.default(getDataFile())
   if (!data) {
+    const fileStats = await fs.stat(getDataFile())
+    console.dir(fileStats)
     throw new Error(`No data loaded from ${path.resolve(getDataFile())}`)
   }
   return data
